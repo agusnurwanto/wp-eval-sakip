63a401ba64e0604e949c5f7ca4a6ad0788474ff4
Bagus Alvin <bagusalvin224@gmail.com>
2024-09-12 15:19:30 +0700

    fix tambah pengaturan menu rencana aksi pemda

--- public/partials/setting-menu/wp-eval-sakip-setting-rencana-aksi.php
+++ public/partials/setting-menu/wp-eval-sakip-setting-rencana-aksi.php
@@ -27,7 +27,30 @@ $body = '';
 <div class="cetak">
 	<div style="padding: 10px;margin:0 0 3rem 0;">
 	<input type="hidden" value="<?php echo get_option( '_crb_apikey_esakip' ); ?>" id="api_key">
-	<h1 class="text-center" style="margin:3rem;">Halaman Pengaturan Rencana Aksi <br>Tahun Anggaran <?php echo $tahun_anggaran; ?></h1>
+	<h1 class="text-center" style="margin:3rem;">Halaman Pengaturan Rencana Aksi Pemerintah Daerah<br>Tahun Anggaran <?php echo $tahun_anggaran; ?></h1>
+		<div class="d-flex justify-content-center">
+			<div class="card" style="width: 50%;">
+				  <div class="card-body">
+					<form>
+						<div class="form-group">
+							<label for="jadwal-renstra">Pilih Jadwal RPJMD/RPD SI KSATRIA</label>
+							<select class="form-control" id="jadwal-rpjmd">
+							</select>
+							<small class="form-text text-muted">Untuk mendapatkan Pohon Kinerja sesuai jadwal RPJMD/RPD. digunakan diinput Rencana Aksi Pemerintah Daerah.</small>
+						</div>
+						<div class="form-group d-flex">
+							<button onclick="submit_pengaturan_menu_pemda(); return false;" class="btn btn-primary ml-auto">Simpan</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="cetak">
+	<div style="padding: 10px;margin:0 0 3rem 0;">
+	<input type="hidden" value="<?php echo get_option( '_crb_apikey_esakip' ); ?>" id="api_key">
+	<h1 class="text-center" style="margin:3rem;">Halaman Pengaturan Rencana Aksi Perangkat Daerah<br>Tahun Anggaran <?php echo $tahun_anggaran; ?></h1>
 		<div class="d-flex justify-content-center">
 			<div class="card" style="width: 50%;">
 				  <div class="card-body">
@@ -36,13 +59,13 @@ $body = '';
 							<label for="jadwal-renstra">Pilih Jadwal RENSTRA SI KSATRIA</label>
 							<select class="form-control" id="jadwal-renstra">
 							</select>
-							<small class="form-text text-muted">Untuk mendapatkan Pohon Kinerja sesuai jadwal RENSTRA. digunakan diinput Rencana Aksi.</small>
+							<small class="form-text text-muted">Untuk mendapatkan Pohon Kinerja sesuai jadwal RENSTRA. digunakan diinput Rencana Aksi Perangkat Daerah.</small>
 						</div>
 						<div class="form-group">
 							<label for="jadwal-renstra">Pilih Jadwal RENSTRA WP-SIPD</label>
 							<select class="form-control" id="jadwal-renstra-wpsipd">
 							</select>
-							<small class="form-text text-muted">Untuk mendapatkan Sasaran Cascading di WP-SIPD sesuai jadwal yang digunakan di input Rencana Aksi.</small>
+							<small class="form-text text-muted">Untuk mendapatkan Sasaran Cascading di WP-SIPD sesuai jadwal yang digunakan di input Rencana Aksi Perangkat Daerah.</small>
 						</div>
 						<div class="form-group d-flex">
 							<button onclick="submit_pengaturan_menu(); return false;" class="btn btn-primary ml-auto">Simpan</button>
@@ -79,9 +102,11 @@ $body = '';
 				if (response.status === 'success') {
 					jQuery('#jadwal-renstra').html(response.option_renstra)
 					jQuery('#jadwal-renstra-wpsipd').html(response.option_renstra_wpsipd)
+					jQuery('#jadwal-rpjmd').html(response.option_rpjmd)
 					if(response.data.length != 0){
 						console.log(response.data.id_jadwal)
 						jQuery('#jadwal-renstra').val(response.data.id_jadwal);
+						jQuery('#jadwal-rpjmd').val(response.data.id_jadwal);
 						if(response.data.id_jadwal_wp_sipd !== null){
 							jQuery('#jadwal-renstra-wpsipd').val(response.data.id_jadwal_wp_sipd);
 						}
@@ -116,6 +141,44 @@ $body = '';
 					'api_key': esakip.api_key,
 					'id_jadwal_renstra': id_jadwal_renstra,
 					'id_jadwal_renstra_wpsipd': id_jadwal_renstra_wpsipd,
+					'tahun_anggaran': tahun_anggaran
+				},
+				success: function(response) {
+					console.log(response);
+					jQuery('#wrap-loading').hide();
+					if (response.status === 'success') {
+						alert(response.message);
+						get_data_pengaturan_rencana_aksi();
+						afterSubmitForm();
+					} else {
+						alert(response.message);
+					}
+				},
+				error: function(xhr, status, error) {
+					console.error(xhr.responseText);
+					alert('Terjadi kesalahan saat mengirim data!');
+					jQuery('#wrap-loading').hide();
+				}
+			});
+		}
+    }
+
+    function submit_pengaturan_menu_pemda(){
+		let id_jadwal_rpjmd = jQuery("#jadwal-rpjmd").val();
+        if (id_jadwal_rpjmd == '') {
+            return alert('Ada data yang kosong!');
+        }
+
+		if(confirm("Apakah kamu yakin untuk mengubah data pokin di Rencana Aksi?\n\nPokin di Rencana Aksi yang sudah ada juga harus diperbarui!")){
+			jQuery('#wrap-loading').show();
+			jQuery.ajax({
+				method: 'POST',
+				url: esakip.url,
+				dataType: 'json',
+				data:{
+					'action': 'submit_pengaturan_rencana_aksi_pemda',
+					'api_key': esakip.api_key,
+					'id_jadwal_rpjmd': id_jadwal_rpjmd,
 					'tahun_anggaran': tahun_anggaran
 				},
 				success: function(response) {
