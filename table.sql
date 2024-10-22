fc7c61bbe76721ae28187fc98210b51dcab15dca
dzawata <interisty91@gmail.com>
2024-10-22 08:41:22 +0700

    add integrasi esr dokumen lkjip lppd pemda

--- table.sql
+++ table.sql
@@ -682,6 +682,8 @@ CREATE TABLE `esakip_lkjip_lppd_pemda` (
   `tahun_anggaran` year(4) DEFAULT NULL,
   `created_at` datetime DEFAULT current_timestamp,
   `active` tinyint(4) DEFAULT 1,
+  `upload_id` int(11) DEFAULT NULL,
+  `path_esr` text DEFAULT NULL,
  PRIMARY KEY(id)
 );
 
