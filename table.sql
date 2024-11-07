2f7efa1ae6f4738c6512bd9e778410657dcff732
dzawata <interisty91@gmail.com>
2024-11-07 15:03:05 +0700

    fix integrasi esr dokumen iku perangkat daerah

--- table.sql
+++ table.sql
@@ -45,6 +45,8 @@ CREATE TABLE `esakip_iku` (
   `tahun_anggaran` year(4) DEFAULT NULL,
   `created_at` datetime DEFAULT current_timestamp,
   `active` tinyint(4) DEFAULT 1,
+  `upload_id` int(11) DEFAULT NULL,
+  `path_esr` text DEFAULT NULL,
  PRIMARY KEY(id)
 );
 
