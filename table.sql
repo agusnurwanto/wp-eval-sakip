22de7224d46999cca9dc22b5774b08b18326d058
dzawata <interisty91@gmail.com>
2024-11-08 14:16:07 +0700

    fix integrasi esr dokumen renja/rkt preangkat daerah

--- table.sql
+++ table.sql
@@ -161,6 +161,8 @@ CREATE TABLE `esakip_renja_rkt` (
   `tahun_anggaran` year(4) DEFAULT NULL,
   `created_at` datetime DEFAULT current_timestamp,
   `active` tinyint(4) DEFAULT 1,
+  `upload_id` int(11) DEFAULT NULL,
+  `path_esr` text DEFAULT NULL,
  PRIMARY KEY(id)
 );
 
